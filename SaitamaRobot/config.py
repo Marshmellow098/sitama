62c8c5a311885fa4ae2ccf5a74dfcfd5bc699cf0
Marshmellow098 <74300487+Marshmellow098@users.noreply.github.com>
2021-01-06 14:03:17 +0530

    Update and rename sample_config.py to config.py

--- SaitamaRobot/config.py
+++ SaitamaRobot/config.py
@@ -17,12 +17,12 @@ class Config(object):
 
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
-    TOKEN = "BOT_TOKEN"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    TOKEN = "1577591352:AAE0_7rK1ppCsE3K_1xur1QxJwTeDicYn_A"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "Sawada"
-    SUPPORT_CHAT = 'OnePunchSupport'  #Your own group for support, do not add the @
-    JOIN_LOGGER = -1001253661229  #Prints any new group the bot is added to, prints just the name and ID.
-    EVENT_LOGS = -1001190806654  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
+    SUPPORT_CHAT = 'testjsksndoene'  #Your own group for support, do not add the @
+    JOIN_LOGGER = -465542539  #Prints any new group the bot is added to, prints just the name and ID.
+    EVENT_LOGS = -465542539  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     #RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'something://somewhat:user@hosturl:port/databasename'  # needed for any database modules
